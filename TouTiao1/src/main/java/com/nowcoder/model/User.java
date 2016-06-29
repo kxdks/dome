2cfefb72b05c13df514b32abda244ef0850b1090
hxj <291379759@qq.com>
2016-06-29 20:40:05 +0800

    first

---
+++ TouTiao1/src/main/java/com/nowcoder/model/User.java
@@ -0,0 +1,22 @@
+package com.nowcoder.model;
+
+/**
+ * Created by Administrator on 2016/6/29.
+ */
+public class User {
+
+
+    private  String username;
+    public  User(String username){
+        this.username=username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
---
+++ TouTiao1/src/main/java/com/nowcoder/service/TouTiaoService.java
@@ -0,0 +1,14 @@
+package com.nowcoder.service;
+
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2016/6/29.
+ */
+@Service
+public class TouTiaoService {
+    public String say(){
+        return "this is toutiao";
+    }
+}
